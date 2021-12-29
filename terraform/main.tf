25246a0ff6a923fdf46424754ecb721a0dd856ef
MattWhite-personal <74813866+MattWhite-personal@users.noreply.github.com>
2021-12-29 09:46:22 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ data "azurerm_resource_group" "rg" {
 
 data "azurerm_dns_zone" "dns-zone" {
   name                = var.DOMAIN
-  resource_group_name = data.rg
+  resource_group_name = data.azurerm_resource_group.rg
 }
 
 resource "azurerm_storage_account" "stmtasts" {
